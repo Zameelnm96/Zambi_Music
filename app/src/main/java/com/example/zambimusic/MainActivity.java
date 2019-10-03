82990ad5e9041eba75859179e96527d7e3c7ad56
Zameel <Zameel@gmail.com>
2019-10-03 15:48:00 +0530

    Runtime permission Added

--- app/src/main/java/com/example/zambimusic/MainActivity.java
+++ app/src/main/java/com/example/zambimusic/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.zambimusic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private  static final int REQUEST_PERMISSION = 100;
+    private  static final String[] PERMISSIONS = {Manifest.permission.READ_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            ((ActivityManager)this.getSystemService(ACTIVITY_SERVICE)).clearApplicationUserData();
+            recreate();
+        }
+        else{
+            onResume();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES. M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED ){
+            requestPermissions(PERMISSIONS,REQUEST_PERMISSION);
+            return;
+        }
     }
 }
